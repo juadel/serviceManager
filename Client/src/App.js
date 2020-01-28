ce2a3e7ffcdbcc38d4e98edab977e809eaee5f07
Juan Delgado <juadel@hotmail.com>
2020-01-27 21:03:35 -0700

    react client

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
+import Customer from  './Customer/Customers';
+import Services from './Services/Services';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Welcome to Service Manager
+      </h1>
+      <h2>
+        Please log in to access <Button>Login</Button>
+      </h2>
     </div>
   );
+}
 }
 
 export default App;
