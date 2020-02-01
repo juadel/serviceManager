157fe9a78e884ede210498209e171764ad5b2a23
Juan Delgado <juadel@hotmail.com>
2020-02-01 00:33:06 -0700

    getitembyID grapped jwtToken

--- Client/src/Services/GetServicebyId.js
+++ Client/src/Services/GetServicebyId.js
@@ -2,70 +2,46 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import { Auth } from 'aws-amplify';
 import ReactLoading from 'react-loading'
-import {Media} from 'react-bootstrap';
+import { Media, Form, FormGroup, FormControl, Button} from 'react-bootstrap';
 
 
 class Service extends Component {
 
 
    
-   constructor(props){
+   constructor(){
        super();
        this.state ={
+           jwtToken :"",
            isLoading: true,
-           ticket:[],
-           
+           ticket:[]
        };
        this.getService('1');
    }
    
-   getService(serviceid){
+   async getService(serviceid){
 
-        Auth.currentSession().then(data => {axios.get('https://b1h983jr2c.execute-api.ca-central-1.amazonaws.com/dev/item/'+serviceid+'?item=service',
-                                                    {headers: { 'Content-Type': 'application/json',
-                                                    'Authorization': `Bearer ${data.idToken.jwtToken}`
-                                            }}).then(res => {this.setState({isLoading: false, ticket :res.data.ticket})
-                                                });
+        await Auth.currentSession().then( data => this.setState({jwtToken:data.idToken.jwtToken})).catch(e => console.log(e))
+        await axios.get('https://b1h983jr2c.execute-api.ca-central-1.amazonaws.com/dev/item/'+serviceid+'?item=service', {headers: 
+                    { 'Content-Type': 'application/json',
+                      'Authorization': `Bearer ${this.state.jwtToken}`}}
+                      ).then(res => {this.setState({isLoading: false, ticket :res.data.ticket})})
+                      .catch(e => console.log(e))
+        console.log(this.state.ticket[0].Title)                                        
                                             
-                                            
-                                        })
-        
-        
-        // Auth.currentSession().then(data => {this.setState({ticket: [], jwtToken : data.idToken.jwtToken});
-        //                                     console.log(this.state.jwtToken)})
-        
-        // axios.get('https://b1h983jr2c.execute-api.ca-central-1.amazonaws.com/dev/item/'+serviceid+'?item=service',
-        //             {headers: { 'Content-Type': 'application/json',
-        //                         'Authorization': `Bearer ${this.state.jwtToken}`
-        //                         }}
-        //         ).then(res => this.setState({ticket : res.data.ticket})).catch(err => console.log(err));
-           
-        // };
-   }
+                                                
+        }
+     
+                                        
+   
    
     render() 
-    {   
-        const serviceTicket = this.state.ticket.map((ticket)=> 
-            <Media key={ticket.ServiceID}>
-                <Media.left>
-                    <a href={ticket.attachmentUrl}>
-                        <img width={64} height={64} src={ticket.attachmentUrl}
-                        alt="Image"/>
-                    </a>
-                </Media.left>
-                <Media.Body>
-                    <Media.Heading>{ticket.Title}</Media.Heading>
-                    <p>Description: {ticket.Description}</p>
-                </Media.Body>
-            </Media>
-        )
+    {    const title = this.state.ticket
+    
         
         return (
             <div>
-                {this.state.isLoading && 
-                            <ReactLoading type="spinningBubbles" color="#444"/>
-                }
-                {serviceTicket}
+                <h1>{title}</h1>
             </div>
             
         )
